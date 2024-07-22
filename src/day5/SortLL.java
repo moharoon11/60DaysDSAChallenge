67b881f7ae46daae0c133bad0284e7083d46c959
moharoon11 <moharoon11107@gmail.com>
2024-07-22 18:08:57 +0530

    DeleteMiddle, RemoveNthNodeFromBack, SortLL

--- src/day5/SortLL.java
+++ src/day5/SortLL.java
@@ -30,4 +30,53 @@ public class SortLL {
         }
         return head;
     }
+
+
+    // optimal approach
+    public ListNode sortList1(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode middle = findMiddle(head);
+        ListNode secondHead = middle.next;
+        middle.next = null;
+        ListNode leftHead = sortList1(head);
+        ListNode rightHead = sortList1(secondHead);
+
+        return merge(leftHead, rightHead);
+    }
+
+    public ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+        dummyNode.next = temp;
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+
+        if(list1 != null) {
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+
+        return dummyNode.next;
+    }
 }

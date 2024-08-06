3ac48a060d06e0aa397b70f08f53107c63274e77
moharoon11 <moharoon11107@gmail.com>
2024-08-06 20:21:16 +0530

    Day14

---
+++ src/day14/AsteroidsCollision.java
@@ -0,0 +1,30 @@
+package day14;
+
+import java.util.ArrayList;
+
+public class AsteroidsCollision {
+
+    public int[] asteroidCollision(int[] asteroids) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for(int i=0; i<asteroids.length; i++) {
+
+            if(asteroids[i] > 0) {
+                list.add(asteroids[i]);
+            } else {
+
+                while(!list.isEmpty() && list.get(list.size() - 1) > 0 && list.get(list.size()-1) < Math.abs(asteroids[i])) {
+                    list.remove(list.size()-1);
+                }
+
+                if(!list.isEmpty() && list.get(list.size() - 1) == Math.abs(asteroids[i])) {
+                    list.remove(list.size() - 1);
+                } else if(list.isEmpty() || list.get(list.size() - 1) < 0) {
+                    list.add(asteroids[i]);
+                }
+            }
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}

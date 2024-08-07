21b5485b0940cf5f69c6979ef4b29bfbe02855cc
moharoon11 <moharoon11107@gmail.com>
2024-08-07 16:47:55 +0530

    day15

--- src/day15/SumOfSubArrayMinimum.java
+++ src/day15/SumOfSubArrayMinimum.java
@@ -1,7 +1,11 @@
 package day15;
 
+import java.util.Stack;
+
 public class SumOfSubArrayMinimum {
 
+
+    // brute force solution
     public int sumSubarrayMins(int[] arr) {
         int sum = 0;
         int mod = (int) Math.pow(10, 9) + 7;
@@ -16,4 +20,54 @@ public class SumOfSubArrayMinimum {
         return sum;
     }
 
+    // optimal solution
+
+    public int sumSubarrayMins1(int[] arr) {
+        long sum = 0;
+        int mod = (int) Math.pow(10, 9) + 7;
+        int[] previousSmaller = previousSmallerIndex(arr);
+        int[] nextSmaller = nextSmallerIndex(arr);
+
+        for(int i=0; i<arr.length; i++) {
+            int left = i - previousSmaller[i];
+            int right = nextSmaller[i] - i;
+            sum = (sum + ((long)right * left * arr[i]) % mod) % mod;
+        }
+
+        return (int) sum;
+    }
+
+    public int[] nextSmallerIndex(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] nextSmaller = new int[n];
+        for(int i=n-1; i>=0; i--) {
+
+            while(!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+
+            nextSmaller[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        return nextSmaller;
+    }
+
+    public int[] previousSmallerIndex(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] previousSmaller = new int[n];
+
+        for(int i=0; i<n; i++) {
+
+            while(!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                stack.pop();
+            }
+
+            previousSmaller[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+
+        return previousSmaller;
+    }
 }
--- src/day15/SumOfSubarrayRanges.java
+++ src/day15/SumOfSubarrayRanges.java
@@ -1,5 +1,7 @@
 package day15;
 
+import java.util.Stack;
+
 public class SumOfSubarrayRanges {
 
     public long subArrayRanges(int[] nums) {
@@ -16,4 +18,102 @@ public class SumOfSubarrayRanges {
         }
         return sum;
     }
+
+    // using stacks
+    public long subArrayRanges1(int[] nums) {
+        return sumSubarrayMaxs(nums) - sumSubarrayMins(nums);
+    }
+
+    public long sumSubarrayMins(int[] arr) {
+        long sum = 0;
+        int mod = (int) Math.pow(10, 9) + 7;
+        int[] previousSmaller = previousSmallerIndex(arr);
+        int[] nextSmaller = nextSmallerIndex(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            int left = i - previousSmaller[i];
+            int right = nextSmaller[i] - i;
+            long product = ((long) right * left % mod) * arr[i] % mod;
+            sum = (sum + product) % mod;
+        }
+
+        return sum;
+    }
+
+    public int[] nextSmallerIndex(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] nextSmaller = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            nextSmaller[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        return nextSmaller;
+    }
+
+    public int[] previousSmallerIndex(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] previousSmaller = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                stack.pop();
+            }
+            previousSmaller[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        return previousSmaller;
+    }
+
+    public long sumSubarrayMaxs(int[] arr) {
+        long sum = 0;
+        int mod = (int) Math.pow(10, 9) + 7;
+        int[] previousLarger = previousLargerIndex(arr);
+        int[] nextLarger = nextLargerIndex(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            int left = i - previousLarger[i];
+            int right = nextLarger[i] - i;
+            long product = ((long) right * left % mod) * arr[i] % mod;
+            sum = (sum + product) % mod;
+        }
+
+        return sum;
+    }
+
+    public int[] nextLargerIndex(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] nextLarger = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
+                stack.pop();
+            }
+            nextLarger[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        return nextLarger;
+    }
+
+    public int[] previousLargerIndex(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] previousLarger = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                stack.pop();
+            }
+            previousLarger[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        return previousLarger;
+    }
+
 }

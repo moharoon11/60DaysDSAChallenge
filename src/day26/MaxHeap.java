061d15111e8bbc972dcc23d960c8f60c029105ec
moharoon11 <moharoon11107@gmail.com>
2024-08-21 14:05:28 +0530

    day26 -> Heap

--- src/day26/MaxHeap.java
+++ src/day26/MaxHeap.java
@@ -1,13 +1,15 @@
 package day26;
 
+import java.lang.Comparable;
 import java.util.ArrayList;
 
-public class Heap<T extends Comparable<T>> {
+
+public class MaxHeap<T extends Comparable<T>> {
 
     private ArrayList<T> list;
 
-    public Heap() {
-        list = new ArrayList<T>();
+    public MaxHeap() {
+        list = new ArrayList<>();
     }
 
     public void swap(int first, int second) {
@@ -21,11 +23,11 @@ public class Heap<T extends Comparable<T>> {
     }
 
     public int left(int index) {
-        return index * 2 + 1;
+        return (index * 2) + 1;
     }
 
     public int right(int index) {
-        return index * 2 + 2;
+        return (index * 2) + 2;
     }
 
     public void insert(T value) {
@@ -37,7 +39,8 @@ public class Heap<T extends Comparable<T>> {
         if(index == 0) return;
 
         int p = parent(index);
-        if(list.get(index).compareTo(list.get(p)) < 0) {
+
+        if(list.get(index).compareTo(list.get(p)) > 0) {
             swap(index, p);
             upHeap(p);
         }
@@ -45,49 +48,59 @@ public class Heap<T extends Comparable<T>> {
 
     public T remove() throws Exception {
         if(list.isEmpty()) {
-            throw new Exception("No elements to remove");
+            throw new Exception("List is empty");
         }
 
-        T first = list.get(0);
+        T temp = list.get(0);
         T last = list.get(list.size() - 1);
+        list.set(0, last);
+        list.remove(list.size() - 1);
 
         if(!list.isEmpty()) {
-           list.set(0, last);
-           downHeap(0);
+            downHeap(0);
         }
 
-        return first;
+        return temp;
     }
 
     public void downHeap(int index) {
-        int min = index;
+        int max = index;
         int left = left(index);
         int right = right(index);
 
-        if(left < list.size() && list.get(left).compareTo(list.get(index)) < 0) {
-            min = left;
+        if(left < list.size() && list.get(left).compareTo(list.get(max)) > 0) {
+            max = left;
         }
 
-        if(right < list.size() && list.get(right).compareTo(list.get(index)) < 0) {
-            min = right;
+        if(right < list.size() && list.get(right).compareTo(list.get(max)) > 0) {
+            max = right;
         }
 
-        if(min != index) {
-            swap(min, index);
-            downHeap(min);
+        if(index != max) {
+            swap(index, max);
+            downHeap(max);
         }
     }
 
+
     public ArrayList<T> heapSort() throws Exception {
         ArrayList<T> data = new ArrayList<>();
+
         while(!list.isEmpty()) {
             data.add(this.remove());
         }
+
         return data;
+    }
+
+    public T extractMax() throws Exception {
+        if(list.isEmpty()) {
+            throw new Exception("List is empty");
+        }
+        return this.remove();
     }
 
     public static void main(String[] args) {
 
     }
-
 }

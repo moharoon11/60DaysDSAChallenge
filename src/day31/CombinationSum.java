cbb72911dd1c0e2324ade6df78d4a83db89d853c
moharoon11 <moharoon11107@gmail.com>
2024-08-28 22:39:51 +0530

    Day30->Recursion

---
+++ src/day31/CombinationSum.java
@@ -0,0 +1,36 @@
+package day31;
+
+import java.util.*;
+
+public class CombinationSum {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> results = new ArrayList<>();
+        helper(candidates, target, 0, new ArrayList<>(), results);
+        return results;
+    }
+
+    public void helper(int[] candidate, int target, int index, List<Integer> current, List<List<Integer>> result) {
+
+        if(target == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        if(index == candidate.length || target < 0) {
+            return;
+        }
+
+        current.add(candidate[index]);
+        helper(candidate, target - candidate[index], index, current, result);
+
+        current.remove(current.size() - 1);
+        helper(candidate, target, index + 1, current, result);
+    }
+
+    public static void main(String[] args) {
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+       //  Output: [[2,2,3],[7]]
+    }
+}
---
+++ src/day31/IsSubsequenceWithSumK.java
@@ -0,0 +1,26 @@
+package day31;
+
+public class IsSubsequenceWithSumK {
+
+
+    // Recursive Solution TLE ERROR
+    public static boolean isSubsetPresent(int n, int k,int []a) {
+        return solve(a, n, k, 0);
+    }
+
+    public static boolean solve(int[] arr, int n, int sum, int index) {
+
+        if(sum == 0) return true;
+
+        if(index == n) return false;
+
+        return (solve(arr, n, sum - arr[index], index + 1) || solve(arr, n, sum, index + 1));
+    }
+
+    public static void main(String[] args) {
+        int n = 3, k = 5;
+        int[] arr = {1, 2, 3};
+    }
+
+    // Optimal solution will be using dynamic programming
+}

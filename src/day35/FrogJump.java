b8c9988bb4cb4893dda8cdae53497f67b2f7a3c3
mohamed_haroon <moharoon11107@gmail.com>
2024-10-09 07:46:11 +0530

    Dynamic Programming

--- src/day35/FrogJump.java
+++ src/day35/FrogJump.java
@@ -62,4 +62,50 @@ public class FrogJump {
 
         return dp[index] = Math.min(left, right);
     }
+
+    // tabulation
+    public int minimumEnergy(int arr[],int N){
+
+        int[] dp = new int[N];
+
+        dp[0] = 0;
+
+        for(int i=1; i<N; i++) {
+            int fs = dp[i-1] + Math.abs(arr[i] - arr[i-1]);
+            int ss = Integer.MAX_VALUE;
+            if(i > 1) {
+                ss = dp[i-2] + Math.abs(arr[i] - arr[i-2]);
+            }
+
+            dp[i] = Math.min(fs, ss);
+        }
+
+        return dp[N-1];
+    }
+
+    // space optimization
+    public int optimal(int arr[],int N){
+
+
+        int prev1 = 0;
+        int prev2 = 0;
+
+        for(int i=1; i<N; i++) {
+            int fs = prev1 + Math.abs(arr[i] - arr[i-1]);
+            int ss = Integer.MAX_VALUE;
+            if(i > 1) {
+                ss = prev2 + Math.abs(arr[i] - arr[i-2]);
+            }
+
+            int current = Math.min(fs, ss);
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 10};
+    }
 }
